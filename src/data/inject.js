b6282cda844a2ba465ec11c20e7ffe590018252d
Brian Girko <brian.girko@gmail.com>
2021-01-10 10:18:43 +0100

    fixes #3

--- src/data/inject.js
+++ src/data/inject.js
@@ -1,40 +1,60 @@
 'use strict';
 
 const script = document.createElement('script');
+script.addEventListener('change', () => chrome.runtime.emit({
+  method: 'change'
+}));
+script.addEventListener('check', () => chrome.runtime.emit({
+  method: 'check'
+}));
 script.textContent = `
-  Object.defineProperty(document, 'visibilityState', {
-    get() {
-      return 'visible';
-    }
-  });
-  Object.defineProperty(document, 'hidden', {
-    get() {
-      return false;
-    }
-  });
   {
+    const script = document.currentScript;
+
+    const check = () => script.dispatchEvent(new Event('check'));
+
+    Object.defineProperty(document, 'visibilityState', {
+      get() {
+        check();
+        return 'visible';
+      }
+    });
+    Object.defineProperty(document, 'hidden', {
+      get() {
+        check();
+        return false;
+      }
+    });
+
     const block = e => {
       e.preventDefault();
       e.stopPropagation();
       e.stopImmediatePropagation();
+
+      script.dispatchEvent(new Event('change'));
     };
     document.addEventListener('visibilitychange', block, true);
     document.addEventListener('webkitvisibilitychange', block, true);
     document.addEventListener('mozvisibilitychange', block, true);
-  }
-  if (/Firefox/.test(navigator.userAgent)) {
-    Object.defineProperty(document, 'mozHidden', {
-      get() {
-        return false;
-      }
-    });
-  }
-  else {
-    Object.defineProperty(document, 'webkitHidden', {
-      get() {
-        return false;
-      }
-    });
+    document.addEventListener('blur', block, true);
+    window.addEventListener('blur', block, true);
+
+    if (/Firefox/.test(navigator.userAgent)) {
+      Object.defineProperty(document, 'mozHidden', {
+        get() {
+          check();
+          return false;
+        }
+      });
+    }
+    else {
+      Object.defineProperty(document, 'webkitHidden', {
+        get() {
+          check();
+          return false;
+        }
+      });
+    }
   }
 `;
 document.documentElement.appendChild(script);

040c18aa60e3f76f1b684af640ad1846b56d8273
Brian Girko <brian.girko@gmail.com>
2021-08-01 01:12:41 -0400

    v2 - v0.1.6, v3 - v0.2.2

--- src/v2/data/options/index.js
+++ src/v2/data/options/index.js
@@ -4,23 +4,48 @@ chrome.storage.local.get({
   'visibilityState': true,
   'hidden': true,
   'blur': true,
-  'mouseleave': true
+  'focus': true,
+  'visibility': true,
+  'mouseleave': true,
+  'log': false,
+  'faqs': true,
+  'policies': {
+    'docs.google.com': ['blur']
+  }
 }, prefs => {
   document.getElementById('visibilityState').checked = prefs.visibilityState;
   document.getElementById('hidden').checked = prefs.hidden;
+  document.getElementById('focus').checked = prefs.focus;
+  document.getElementById('visibility').checked = prefs.visibility;
   document.getElementById('blur').checked = prefs.blur;
   document.getElementById('mouseleave').checked = prefs.mouseleave;
+  document.getElementById('log').checked = prefs.log;
+  document.getElementById('faqs').checked = prefs.faqs;
+  document.getElementById('policies').value = JSON.stringify(prefs.policies, null, '  ');
 });
 
-document.getElementById('save').addEventListener('click', () => chrome.storage.local.set({
-  'visibilityState': document.getElementById('visibilityState').checked,
-  'hidden': document.getElementById('hidden').checked,
-  'blur': document.getElementById('blur').checked,
-  'mouseleave': document.getElementById('mouseleave').checked
-}, () => {
-  toast.textContent = 'Options Saved';
-  setTimeout(() => toast.textContent = '', 1000);
-}));
+document.getElementById('save').addEventListener('click', () => {
+  try {
+    chrome.storage.local.set({
+      'visibilityState': document.getElementById('visibilityState').checked,
+      'hidden': document.getElementById('hidden').checked,
+      'blur': document.getElementById('blur').checked,
+      'mouseleave': document.getElementById('mouseleave').checked,
+      'visibility': document.getElementById('visibility').checked,
+      'focus': document.getElementById('focus').checked,
+      'log': document.getElementById('log').checked,
+      'faqs': document.getElementById('faqs').checked,
+      'policies': JSON.parse(document.getElementById('policies').value)
+    }, () => {
+      toast.textContent = 'Options Saved';
+      setTimeout(() => toast.textContent = '', 1000);
+    });
+  }
+  catch (e) {
+    toast.textContent = e.message;
+    setTimeout(() => toast.textContent = '', 5000);
+  }
+});
 // reset
 document.getElementById('reset').addEventListener('click', e => {
   if (e.detail === 1) {
@@ -39,3 +64,18 @@ document.getElementById('reset').addEventListener('click', e => {
 document.getElementById('support').addEventListener('click', () => chrome.tabs.create({
   url: chrome.runtime.getManifest().homepage_url + '?rd=donate'
 }));
+
+// report
+document.getElementById('report').addEventListener('click', () => chrome.tabs.create({
+  url: chrome.runtime.getManifest().homepage_url + '#reviews'
+}));
+
+// links
+const links = window.links = (d = document) => {
+  for (const a of [...d.querySelectorAll('[data-href]')]) {
+    if (a.hasAttribute('href') === false) {
+      a.href = chrome.runtime.getManifest().homepage_url + '#' + a.dataset.href;
+    }
+  }
+};
+document.addEventListener('DOMContentLoaded', () => links());
--- src/v3/data/inject.js
+++ src/v3/data/inject.js
@@ -9,36 +9,36 @@ script.addEventListener('check', () => chrome.runtime.sendMessage({
 }));
 script.textContent = `{
   const script = document.currentScript;
+  const isFirefox = /Firefox/.test(navigator.userAgent) || typeof InstallTrigger !== 'undefined';
 
   const check = () => script.dispatchEvent(new Event('check'));
+  const block = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    e.stopImmediatePropagation();
 
+    check();
+  };
+
+  /* visibility */
   Object.defineProperty(document, 'visibilityState', {
     get() {
       check();
       return 'visible';
     }
   });
+  document.addEventListener('visibilitychange', e => script.dataset.visibility !== 'false' && block(e), true);
+  document.addEventListener('webkitvisibilitychange', e => script.dataset.visibility !== 'false' && block(e), true);
+  document.addEventListener('mozvisibilitychange', e => script.dataset.visibility !== 'false' && block(e), true);
+
+  /* hidden */
   Object.defineProperty(document, 'hidden', {
     get() {
       check();
       return false;
     }
   });
-
-  const block = e => {
-    e.preventDefault();
-    e.stopPropagation();
-    e.stopImmediatePropagation();
-
-    script.dispatchEvent(new Event('change'));
-  };
-  document.addEventListener('visibilitychange', block, true);
-  document.addEventListener('webkitvisibilitychange', block, true);
-  document.addEventListener('mozvisibilitychange', block, true);
-  document.addEventListener('hasFocus', block, true);
-  document.__proto__.hasFocus = function() {return true};
-
-  if (/Firefox/.test(navigator.userAgent)) {
+  if (isFirefox) {
     Object.defineProperty(document, 'mozHidden', {
       get() {
         check();
@@ -54,24 +54,41 @@ script.textContent = `{
       }
     });
   }
+
+  /* focus */
+  document.addEventListener('hasFocus', e => script.dataset.focus !== 'false' && block(e), true);
+  document.__proto__.hasFocus = function() {return true};
+
+  /* blur */
   document.addEventListener('blur', e => script.dataset.blur !== 'false' && block(e), true);
   window.addEventListener('blur', e => script.dataset.blur !== 'false' && block(e), true);
+
+  /* mouse*/
   window.addEventListener('mouseleave', e => script.dataset.mouseleave !== 'false' && block(e), true);
 }`;
 document.documentElement.appendChild(script);
 script.remove();
-chrome.storage.local.get({
+const update = () => chrome.storage.local.get({
   'blur': true,
-  'mouseleave': true
+  'focus': true,
+  'mouseleave': true,
+  'visibility': true,
+  'policies': {
+    'docs.google.com': ['blur']
+  }
 }, prefs => {
-  script.dataset.blur = prefs.blur;
-  script.dataset.mouseleave = prefs.mouseleave;
-});
-chrome.storage.onChanged.addListener(prefs => {
-  if (prefs.blur) {
-    script.dataset.blur = prefs.blur.newValue;
+  let hostname = location.hostname;
+  try {
+    hostname = parent.location.hostname;
   }
-  if (prefs.mouseleave) {
-    script.dataset.mouseleave = prefs.mouseleave.newValue;
-  }
+  catch (e) {}
+
+  const policy = prefs.policies[hostname] || [];
+
+  script.dataset.blur = policy.indexOf('blur') === -1 ? prefs.blur : false;
+  script.dataset.focus = policy.indexOf('focus') === -1 ? prefs.focus : false;
+  script.dataset.mouseleave = policy.indexOf('mouseleave') === -1 ? prefs.mouseleave : false;
+  script.dataset.visibility = policy.indexOf('visibility') === -1 ? prefs.visibility : false;
 });
+update();
+chrome.storage.onChanged.addListener(update);
